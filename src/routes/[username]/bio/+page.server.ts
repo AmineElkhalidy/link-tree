66c30ecfa28f6f7417f5840fa5cb699872726d19
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-01-22 13:43:28 +0100

    Changes added!

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,46 +1,24 @@
-import type { PageServerLoad, Actions } from "./$types";
+import type { PageServerLoad } from "./$types";
 import { adminAuth, adminDB } from "$lib/server/admin";
-import { error, redirect, fail } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
 
-export const load = (async ({ locals, params }) => {
-  const uid = locals.userID;
+export const load = (async ({ cookies }) => {
+  const sessionCookie = cookies.get("__session");
 
-  if (!uid) {
-    throw redirect(301, "/login");
+  try {
+    const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie!);
+    const userDoc = await adminDB
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    const userData = userDoc.data();
+
+    return {
+      bio: userData?.bio,
+    };
+  } catch (e) {
+    console.log(e);
+    // redirect(301, '/login');
+    throw error(401, "Unauthorized request!");
   }
-
-  const userDoc = await adminDB.collection("users").doc(uid).get();
-  const { username, bio } = userDoc.data()!;
-
-  if (params.username !== username) {
-    throw error(401, "That username does not belong to you");
-  }
-
-  return {
-    bio,
-  };
 }) satisfies PageServerLoad;
-
-export const actions = {
-  default: async ({ locals, request, params }) => {
-    const uid = locals.userID;
-
-    const data = await request.formData();
-    const bio = data.get("bio");
-
-    const userRef = adminDB.collection("users").doc(uid!);
-    const { username } = (await userRef.get()).data()!;
-
-    if (params.username !== username) {
-      throw error(401, "That username does not belong to you");
-    }
-
-    if (bio!.length > 260) {
-      return fail(400, { problem: "Bio must be less than 260 characters" });
-    }
-
-    await userRef.update({
-      bio,
-    });
-  },
-} satisfies Actions;
